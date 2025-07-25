6b0ed525d85f8cba6d0b2963c9ce9586e9d9faac
Jiří Černý <funkacer@gmail.com>
2025-07-25 08:29:24 +0200

    Update store_db_init.py

--- store_db_init.py
+++ store_db_init.py
@@ -1,5 +1,5 @@
 import sqlite3
-con = sqlite3.connect("froshims.db")
-con.execute("DROP TABLE IF EXISTS registrants;")
-con.execute("CREATE TABLE registrants (id INTEGER, name TEXT NOT NULL, sport TEXT NOT NULL, sex BOOL, PRIMARY KEY (id));")
+con = sqlite3.connect("store.db")
+con.execute("DROP TABLE IF EXISTS books;")
+con.execute("CREATE TABLE books (id INTEGER, title TEXT NOT NULL, PRIMARY KEY (id));")
 con.close()
